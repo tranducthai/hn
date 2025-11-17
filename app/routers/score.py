a6ad1d21175350c934947c8ec314bb530468068b
thaitran <thaittti1@gmail.com>
2025-11-17 09:46:04 +0700

    fix code

--- app/routers/score.py
+++ app/routers/score.py
@@ -5,15 +5,16 @@ from app.models.score import Score
 
 router = APIRouter()
 
-@router.get("/predict", response_model=str, summary="Get job predict")
-async def get_job_predict(id: List[float] = Query(..., description="Mảng 8 số")):
+@router.post("/predict", response_model=str, summary="Get job predict")
+async def get_job_predict(score: Score):
     """
-    Nhận mảng 8 số qua query param id, trả về 'builder'
+    Nhận body JSON:
+    {
+        "id": [1,2,3,4,5,6,7,8]
+    }
     """
-    if len(id) != 8:
+    if len(score.id) != 8:
         raise HTTPException(status_code=422, detail="Bạn phải truyền đúng 8 số")
 
-    # Tạo instance Score từ query param
-    score_instance = Score(id=id)
-    result = get_job_predict_service.get_job_predict(score_instance)
+    result = get_job_predict_service.get_job_predict(score)
     return result
